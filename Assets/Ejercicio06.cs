f82e92a9585f1ca6cf4cde32cb5303b1af253884
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-03-30 15:51:25 -0300

    Exercises 7 & 8 finished

---
+++ Assets/Ejercicio06.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio06 : MonoBehaviour
+{
+    public int num1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 < 0)
+        {
+            Debug.Log("Por favor ingrese numeros mayores a 0");
+        }
+        else
+        {
+             num1 = num1 % 2;
+            if (num1 == 0)
+            {
+                Debug.Log("El numero ingresado es par");
+            }
+            else{
+                Debug.Log("El numero ingresado es impar");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicio07.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio07 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public int num3;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2 && num1 == num3 && num2 == num3)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+        else if (num1 > num2 && num1 > num3)
+        {
+            Debug.Log("El mayor numero es " + num1);
+        }
+        else if (num2 > num1 && num2 > num3)
+        {
+            Debug.Log("El mayor numero es " + num2);
+        }
+        else if (num3 > num1 && num3 > num2)
+        {
+            Debug.Log("El mayor numero es " + num3);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
