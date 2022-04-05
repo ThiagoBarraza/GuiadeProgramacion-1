b137abf423895c3f4d28eac03c7af82b86cec4e3
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-04-05 08:57:14 -0300

    Exercise 10 finished, 11 started

--- Assets/Ejercicio09.cs
+++ Assets/Ejercicio09.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*9. Realizá un programa que resuelva el siguiente problema:  
+Tres personas aportan diferente capital a una sociedad. Se desea saber qué porcentaje del  total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado  por las tres.  
+Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido en el siguiente formato:  
+Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ 
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,16 +12,16 @@ public class Ejercicio09 : MonoBehaviour
     public string nombre1;
     public string nombre2;
     public string nombre3;
-    public int monto1;
-    public int monto2;
-    public int monto3;
+    public float monto1;
+    public float monto2;
+    public float monto3;
     // Start is called before the first frame update
     void Start()
     {
-        int montotal = monto1 + monto2 + monto3;
-        int porcentaje1;
-        int porcentaje2;
-        int porcentaje3;
+        float montotal = monto1 + monto2 + monto3;
+        float porcentaje1;
+        float porcentaje2;
+        float porcentaje3;
 
         porcentaje1 = monto1 * 100 / montotal;
         porcentaje2 = monto2 * 100 / montotal;
--- Assets/Ejercicio10.cs
+++ Assets/Ejercicio10.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class Ejercicio10 : MonoBehaviour
 {
+    public int horActual;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        int HORA_APERTURA = 10;
+        int HORA_CIERRE = 18;
+        bool estaAbierto = true;
+
+        if (horActual < 0 || horActual > 24)
+        {
+            Debug.Log("Ha ingresado una hora incorrecta");
+        }
+        else
+        {
+            if (horActual < HORA_APERTURA && horActual > HORA_CIERRE)
+            {
+                estaAbierto = false;
+            }
+
+            if (estaAbierto == true)
+            {
+                Debug.Log("El estacionamiento esta abierto");
+            }
+            else
+            {
+                Debug.Log("El estacionamiento esta cerrado");
+            }
+        }
     }
 
     // Update is called once per frame
